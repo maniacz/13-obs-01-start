eb89185a8e762dd583ebe588462fd459c56213ba
Lukasz Kipka <lukasz.kipka@pb.com>
2022-05-17 10:02:49 +0200

    175. Errors & Completion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,13 +21,24 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
         count++;
       },1000)
     })
 
     this.firstObsSubscription = customIntervalObservable.subscribe(data => {
       console.log(data);
-    })
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+      console.log('Completed!');
+    } );
   }
   
   ngOnDestroy(): void {
